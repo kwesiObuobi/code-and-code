d20673cc83eece01b8fc1f52c2e63bb78eabb9fe
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-04-04 12:07:58 +0000

    Add test to check for string.length < 1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const stringLength = (string) => {
-  if (string.length < 1 && string.length <= 10) {
+  if (string.length > 1 && string.length <= 10) {
     return string.length;
   }
   return 'invalid input';
--- main.test.js
+++ main.test.js
@@ -2,6 +2,16 @@ const {stringLength} = require('./main');
 
 describe('String Length', () => {
   it('should return the correct length of the string', () => {
-    expect(stringLength('kwesi')).toBe(5);
-  })
+    const length = stringLength('kwesi') ;
+
+    expect(length).toBe(5);
+  });
+
+  it('should return an error if the string has less than 1 character', () => {
+    const length = stringLength('');
+
+    expect(length).toBe('invalid input');
+  });
+
+  
 })
