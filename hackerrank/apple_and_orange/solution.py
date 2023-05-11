0e526de9617104852d0fb2f510b3853da74f3222
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-11 08:36:17 +0000

    Add solution.py for Apple And Orange challenge

---
+++ hackerrank/apple_and_orange/solution.py
@@ -0,0 +1,19 @@
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+  # Write your code here
+  applesInRange = 0
+  orangesInRange = 0
+  
+  for i in range(len(apples)):
+    if apples[i] + a >= s and apples[i] + a <= t:
+      applesInRange += 1
+          
+  for i in range(len(oranges)):
+    if oranges[i] + b >= s and oranges[i] + b <= t:
+      orangesInRange += 1
+          
+  print(applesInRange)
+  print(orangesInRange)
+
+countApplesAndOranges(7, 10, 4, 12, [2,3,-4], [3,-2,-4]);
+# 1
+# 2
