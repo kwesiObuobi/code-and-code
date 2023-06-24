2e541dbcb7f9a41d9dd55912ee42b22886c83854
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-24 16:26:54 +0000

    Add ruby solution to apples_and_oranges challenge [hackerrank]

---
+++ hackerrank/apple_and_orange/solution.rb
@@ -0,0 +1,15 @@
+def countApplesAndOranges(s, t, a, b, apples, oranges)
+  # Write your code here
+  apples_in_range = 0
+  oranges_in_range = 0
+  
+  apples.each { |apple| apples_in_range += 1 if apple + a >= s && apple + a <= t }
+  oranges.each { |orange| oranges_in_range += 1 if orange + b >= s && orange + b <= t }
+
+  puts apples_in_range
+  puts oranges_in_range
+end
+
+puts(countApplesAndOranges(7, 10, 4, 12, [2,3,-4], [3,-2,-4]))
+# 1
+# 2
