8c6c2dec5e8f35eeedf6924082654024b8a957ec
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-10-18 11:23:38 +0000

    Add hackerrank/camel_case/solution.js

---
+++ hackerrank/camel_case/solution.js
@@ -0,0 +1,16 @@
+function camelcase(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      count++;
+    }
+  }
+  return ++count;
+}
+  
+  console.log(camelcase('oneTwoThree'))
+  // 3
+  
+  console.log(camelcase('saveChangesInTheEditor'))
+  // 5
+  
